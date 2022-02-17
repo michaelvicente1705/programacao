a853f773c61c1f3c5473540162cf64d6ed176a59
Michael Vicente <michael.vicente@engeselt.com.br>
2022-02-16 21:26:20 -0300

    revisao de orientacao a objetos em c# e introducao do linq

---
+++ linq/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The Three Parts of a LINQ Query:
+            // 1. Data source.
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            // 3. Query execution.
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ maior_idade/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> list_pessoa = new List<Pessoa>();
+
+            while (true)
+            {
+                Console.Write("Deseja cadastrar uma pessoa?");
+
+                if (Console.ReadLine() == "sim")
+                {
+
+                    Console.Write("Nome: ");
+                    string nome = Console.ReadLine();
+                    Console.Write("Idade: ");
+                    int idade = Convert.ToInt32(Console.ReadLine());
+                    Pessoa n1 = new Pessoa(nome, idade);
+                    list_pessoa.Add(n1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            if (list_pessoa.Count() != 0 )
+            {
+              int maior = 0;
+              string maiorNome = null;
+              foreach (Pessoa x in list_pessoa)
+                {
+                    if (x.Idade > maior)
+                    {
+                        maior = x.Idade;
+                        maiorNome = x.PessoaNo;
+                    }
+                    Console.WriteLine("A pessoa com a maior idade é: ", maiorNome, "com, ", maior, " anos");
+                    //Console.WriteLine(x.Idade);
+                    //Console.WriteLine(x.PessoaNo);
+                }
+            // list_pessoa.ForEach(pessoa => Console.WriteLine(String.Format("{0} {1} {2} {3} {4} {5}", "Idade:", pessoa.Idade, "Nome:", pessoa.PessoaNo, "ID:", pessoa.Id)));
+                
+            }
+            Console.ReadKey();
+
+
+            //Console.ReadKey();
+        }
+    }
+}
