cf63c9daa95547e016b4f24fcab83dc41f203934
Michael Vicente <michael.vicente@engeselt.com.br>
2022-02-19 12:49:59 -0300

    Ajustando a estrutura de pastas e criando projeto ecommerce

--- Laravel/Ecommerce/routes/web.php
+++ Laravel/Ecommerce/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use \App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +13,4 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, "index"]);
+Route::get('/', [HomeController::class,'index']);
